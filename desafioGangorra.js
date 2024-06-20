6d2f108a2b33ccd96e7cbd9f9e8607a5363fd439
edusouza455 <edu.testes456@gmail.com>
2024-06-20 12:46:06 -0300

    .

---
+++ desafioGangorra.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+// metodo trim para remover os espaços em branco. 
+// metodo split para dividir com base nos espacos em branco.
+// metodo map para pegar os valores da funcao 'parseint()'
+let [P1, C1, P2, C2] = lines.shift().trim().split(' ').map((x) => parseInt(x)); 
+
+
+let diferenca = P2 * C2 - P1 * C1;  
+
+if(diferenca < 0){
+    console.log(-1);
+}else if(diferenca > 0){
+    console.log(1);
+}else{
+    console.log(0);
+}
